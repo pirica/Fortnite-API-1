96c62a999919bf3eba30c5d5fafecd6a92804f3d
Tusta <erwinkulasic@gmail.com>
2019-05-12 02:09:09 +0200

    ':fire:'

--- src/FortniteAPI.js
+++ src/FortniteAPI.js
@@ -15,7 +15,11 @@ class FortniteAPI extends require('./FortniteClient') {
                     placetop10: 0,
                     placetop25: 0,
                     minutesplayed: 0,
-                    lastmodified: 0
+                    lastmodified: 0,
+                    kpm: 0,
+                    spm: 0,
+                    kd: 0,
+                    winrate: 0
                 },
                 duo: {
                     score: 0,
@@ -25,7 +29,11 @@ class FortniteAPI extends require('./FortniteClient') {
                     placetop5: 0,
                     placetop12: 0,
                     minutesplayed: 0,
-                    lastmodified: 0
+                    lastmodified: 0,
+                    kpm: 0,
+                    spm: 0,
+                    kd: 0,
+                    winrate: 0
                 },
                 squad: {
                     score: 0,
@@ -35,12 +43,35 @@ class FortniteAPI extends require('./FortniteClient') {
                     placetop3: 0,
                     placetop6: 0,
                     minutesplayed: 0,
-                    lastmodified: 0
+                    lastmodified: 0,
+                    kpm: 0,
+                    spm: 0,
+                    kd: 0,
+                    winrate: 0
                 }
             };
         } else {
             throw new Error('Incorrect or no access data, please enter the options correctly.');
         }
+    }
+
+    clearResult() {
+         ['solo', 'duo', 'squad'].forEach(modes => {
+            this.result[modes] = {
+                score: 0,
+                kills: 0,
+                matchesplayed: 0,
+                placetop1: 0,
+                placetop3: 0,
+                placetop6: 0,
+                minutesplayed: 0,
+                lastmodified: 0,
+                kpm: 0,
+                spm: 0,
+                kd: 0,
+                winrate: 0
+            }
+         });
     }
 
     getUser(username, platform = 'pc', time = 'alltime') {
@@ -72,11 +103,12 @@ class FortniteAPI extends require('./FortniteClient') {
                             this.result.displayName = getLookup.displayName;
                             this.result.id = getLookup.id;
                             this.result.platform = platform;
+                            this.clearResult();
                             this.parseStats(this.result, getStats, platform);
                             if (this.options.fastFetching)
                                 this.pushUser(this.result);
                             
-                            if(user == undefined && this.options.fastFetching || !this.options.fastFetching)
+                            if((user == undefined && this.options.fastFetching) || !this.options.fastFetching)
                                 resolve(this.result);
                         })
                 }).catch(e => reject(e));
